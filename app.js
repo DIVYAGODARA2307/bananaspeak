dc6aba482e210e7574112188aa5682c6af8c2f17
DIVYAGODARA2307 <112926557+DIVYAGODARA2307@users.noreply.github.com>
2022-09-19 14:26:17 +0530

    welcome msg

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log(script is working from a different File.)
+//input
+var username= prompt("give me your username")
+//processing
+    alert("this script works!"+username)
+    //output
+    alert('welcome!')
